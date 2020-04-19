9e3c7fecea2dc3941aaa9cd548564f02b11ba95a
Takuji Okubo <takuji.okubo@gmail.com>
2020-04-19 18:43:20 +1000

    Enable search by Enter key

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -17,20 +17,30 @@ function Search() {
             })
     }
 
+    const handleKeyPress = e => {
+        if (e.key === "Enter") {
+            handleClick(e);
+        }
+    }
+
     return (
         <div className="container">
             <div className="card">
                 <div className="card-body">
                     <p className="font-weight-bold">Book Search</p>
                     <div className="form-group">
-                        <input type="text" className="form-control" name="keyword" value={keyword}  onChange={e => setKeyword(e.target.value) }/>
+                        <input type="text" className="form-control" name="keyword" 
+                            value={keyword}
+                            onChange={e => setKeyword(e.target.value)}
+                            onKeyPress={handleKeyPress}
+                            />
                     </div>
                     <button type="submit" className="btn btn-dark" onClick={handleClick}>Search</button>
                 </div>
             </div> {/* end Card */}
             <p/>
-           {books.map((book)=>{
-               return <Book data={book} />
+           {books.map((book, index)=>{
+               return <Book key={index} data={book} />
            })}
         </div>
     );

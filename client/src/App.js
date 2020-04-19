1b1629de787261ce9f5822ebffc3a552526964f5
Takuji Okubo <takuji.okubo@gmail.com>
2020-04-19 18:41:31 +1000

    Removed imports which are no longer used

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
 import SearchPage from "./pages/SearchPage";
-import Navbar from "./components/Navbar";
-//import Header from "./components/Header";
-import SearchResults from "./components/Search";
 
 function App() {
   return (

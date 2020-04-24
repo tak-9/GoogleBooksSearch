c152c0667c30ffad0616c5cd9879949564d117d1
Takuji Okubo <takuji.okubo@gmail.com>
2020-04-24 17:28:50 +1000

    update for deploying to netlify

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,8 +3,8 @@ import axios from "axios";
 var serverUrl =  "http://localhost:3001";
 // (change serverUrl on heroku)
 if (process.env.NODE_ENV === "production") {
-    serverUrl =  "";
-}
+    serverUrl =  "https://google-books-takuji.herokuapp.com/";
+} 
 
 export const getBooks = (title) => {
     return new Promise(function(resolve, reject){
